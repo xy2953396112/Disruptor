11e24558ec875bed2f170b44cc212589a18148ab
xzh <953396112@qq.com>
2017-09-13 10:02:03 +0800

    Disruptor Demo

---
+++ src/com/xzh/base/LongEventHandler.java
@@ -0,0 +1,14 @@
+package com.xzh.base;
+
+import com.lmax.disruptor.EventHandler;
+
+//我们还需要一个事件消费者，也就是一个事件处理器。这个事件处理器简单地把事件中存储的数据打印到终端：
+public class LongEventHandler implements EventHandler<LongEvent>  {
+    
+	//触发事件
+	@Override
+	public void onEvent(LongEvent longEvent, long l, boolean b) throws Exception {
+		System.out.println(longEvent.getValue()); 		
+	}
+
+}
---
+++ src/com/xzh/base2/Handler3.java
@@ -0,0 +1,11 @@
+package com.xzh.base2;
+
+import com.xzh.base1.Trade;
+import com.lmax.disruptor.EventHandler;
+
+public class Handler3 implements EventHandler<Trade> {
+    @Override  
+    public void onEvent(Trade event, long sequence,  boolean endOfBatch) throws Exception {  
+    	System.out.println("handler3: name: " + event.getName() + " , price: " + event.getPrice() + ";  instance: " + event.toString());
+    }  
+}
